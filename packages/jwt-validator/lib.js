dee682c94d291127c9deb296d9a16fed9572bd0d
Robert Damphousse <robert.damphousse.devex@okta.com>
2017-08-28 08:18:00 -0700

    Added jwt-validator

---
+++ packages/jwt-validator/lib.js
@@ -0,0 +1,51 @@
+const jwksClient = require('jwks-rsa');
+const nJwt = require('njwt');
+
+class OktaJwtVerifier {
+  constructor(options = {}) {
+
+    // TODO parameter validation
+    this.clientId = options.clientId;
+    this.claimsToAssert = options.assertClaims || {};
+    this.jwksClient = jwksClient({
+      jwksUri: options.issuer + '/v1/keys',
+      cache: true,
+      cacheMaxAge: options.cacheMaxAge || (60 * 60 * 1000),
+      cacheMaxEntries: options.cacheMaxEntries || 10,
+      jwksRequestsPerMinute: options.jwksRequestsPerMinute || 10,
+      rateLimit: true
+    });
+    this.verifier = nJwt.createVerifier().setSigningAlgorithm('RS256').withKeyResolver((kid, cb) => {
+      this.jwksClient.getSigningKey(kid, (err, key) => {
+        cb(err, key && (key.publicKey || key.rsaPublicKey));
+      });
+    });
+  }
+
+  verifyAccessToken(accessTokenString) {
+    const self = this;
+    return new Promise((resolve, reject) => {
+      self.verifier.verify(accessTokenString, (err, jwt) => {
+        if (err) {
+          return reject(err);
+        }
+        jwt.claims = jwt.body;
+        delete jwt.body;
+        const errors = [];
+        for (let claim of Object.keys(self.claimsToAssert)) {
+          const actualValue = jwt.claims[claim];
+          const expectedValue = self.claimsToAssert[claim];
+          if (actualValue !== expectedValue) {
+            errors.push(`claim '${claim}' value '${actualValue}' does not match expected value '${expectedValue}'`);
+          }
+        }
+        if (errors.length) {
+          return reject(new Error(errors.join(', ')));
+        }
+        resolve(jwt);
+      });
+    });
+  }
+}
+
+module.exports = OktaJwtVerifier;
---
+++ packages/jwt-validator/test/util.js
@@ -0,0 +1,56 @@
+const qs = require('qs');
+const request = require('request');
+const url = require('url');
+
+function getAccessToken(options = {}) {
+  const {
+    ISSUER,
+    CLIENT_ID,
+    REDIRECT_URI,
+    USERNAME,
+    PASSWORD
+  } = options;
+
+  return new Promise((resolve, reject) => {
+    const urlProperties = url.parse(ISSUER);
+    const domain = urlProperties.protocol + '//' + urlProperties.host;
+    const postUrl = domain + '/api/v1/authn';
+    request.post(postUrl, {
+      json: true,
+      body: {
+        username: USERNAME,
+        password: PASSWORD
+      }
+    }, function (err, resp, body) {
+      if (err || resp.statusCode >= 400) {
+        return resolve(err || body);
+      }
+      const authorizeParams = {
+        sessionToken: body.sessionToken,
+        response_type: 'token',
+        client_id: CLIENT_ID,
+        redirect_uri: REDIRECT_URI,
+        scope: 'openid',
+        nonce: 'foo',
+        state: 'foo'
+      }
+      const authorizeUrl = ISSUER + '/v1/authorize?' + qs.stringify(authorizeParams);
+      request.get(authorizeUrl, {followRedirect: false}, function(err, resp, body) {
+        const parsedUrl = url.parse(resp.headers.location, true);
+        if (parsedUrl.query.error) {
+          return reject(parsedUrl.query.error);
+        }
+        const match = resp.headers.location.match(/access_token=([^&]+)/);
+        const accessToken = match && match[1];
+        if (!accessToken){
+          return reject(new Error('Could not parse access token from URI'));
+        }
+        resolve(accessToken);
+      })
+    });
+  });
+}
+
+module.exports = {
+  getAccessToken
+};
