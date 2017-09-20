89b2b21db6781ab2ed146c1f4cf9914a8ff7fd54
Len Boyette <lboyette@okta.com>
2017-09-20 15:14:42 -0700

    Fixes for publication (#7)

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,16 +6,7 @@ REGISTRY="${ARTIFACTORY_URL}/api/npm/npm-okta"
 
 export TEST_SUITE_TYPE="build"
 
-if [ -n "$action_branch" ];
-then
-  echo "Publishing from bacon task using branch $action_branch"
-  TARGET_BRANCH=$action_branch
-else
-  echo "Publishing from bacon testSuite using branch $BRANCH"
-  TARGET_BRANCH=$BRANCH
-fi
-
-if [ "$action_branch" == "master" ];
+if [ "$BRANCH" == "master" ];
 then
   echo "attempting npm publish"
   if ! lerna publish --registry ${REGISTRY} --cd-version patch --yes; then
