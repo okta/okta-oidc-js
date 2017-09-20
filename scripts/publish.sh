a1fa1a39d69eddb31b3d7c2724236b37d137b9d6
Len Boyette <lboyette@okta.com>
2017-09-20 15:36:21 -0700

    Checkout master before publish

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -9,6 +9,7 @@ export TEST_SUITE_TYPE="build"
 if [ "$BRANCH" == "master" ];
 then
   echo "attempting npm publish"
+  # NOTE: if any commits are added after this one in master, the git merge will fail
   if ! lerna publish --registry ${REGISTRY} --cd-version patch --yes; then
     echo "npm publish failed! Exiting..."
     exit $PUBLISH_ARTIFACTORY_FAILURE
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,6 +2,13 @@
 
 cd ${OKTA_HOME}/${REPO}
 
+# undo permissions change on scripts/publish.sh
+git checkout -- scripts
+
+# ensure we're in a branch on the correct sha
+git checkout $BRANCH
+git reset --hard $SHA
+
 # Use newer, faster npm
 npm install -g npm@5.0.3
 
