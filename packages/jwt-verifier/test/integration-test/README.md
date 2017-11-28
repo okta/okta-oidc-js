f689054c9bc2e7c70ad7a276f63ffb093bf09cdc
Vijet Mahabaleshwar <vijet.mahabaleshwar@okta.com>
2017-11-28 15:36:15 -0800

    Adding support to run TCK based integration tests for jwt-verifier resource server flow Also updated the tck jar to the latest version

--- packages/jwt-verifier/test/integration-test/README.md
+++ packages/jwt-verifier/test/integration-test/README.md
@@ -5,7 +5,7 @@ We have tested using java version "1.8.0_131"
 
 ## Run integration Tests
 Run the following command to run all the integration tests.
-Ensure you're in the `oidc-middleware` base directory
+Ensure you're in the `jwt-verifier` base directory
 
 ```
 npm run tck-test
---
+++ packages/jwt-verifier/test/integration-test/resource-server.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const OktaJwtVerifier = require('../../lib');
+var cors = require('cors');
+
+const oktaJwtVerifier = new OktaJwtVerifier({
+  issuer: 'http://localhost:9090/oauth2/default',
+  assertClaims: {
+    aud: 'api://default'
+  },
+});
+
+/**
+ * A simple middleware that asserts valid access tokens and sends 401 responses
+ * if the token is not present or fails validation.  If the token is valid its
+ * contents are attached to req.jwt
+ */
+function authenticationRequired(req, res, next) {
+  const authHeader = req.headers.authorization || '';
+  const match = authHeader.match(/Bearer (.+)/);
+
+  if (!match) {
+    res.set('WWW-Authenticate', 'Bearer realm=resource error="unauthorized", error_description="Bearer token not found');
+    return res.status(401).end();
+  }
+
+  const accessToken = match[1];
+
+  return oktaJwtVerifier.verifyAccessToken(accessToken)
+    .then((jwt) => {
+      req.jwt = jwt;
+      next();
+    })
+    .catch((err) => {
+      const header = 'Bearer realm=resource, error="unauthorized", error_description="' + err.message + '"';
+      res.set('WWW-Authenticate', header);
+      res.status(401).send(err.message);
+    });
+}
+
+const app = express();
+
+/**
+ * For local testing only!  Enables CORS for all domains
+ */
+app.use(cors());
+
+/**
+ * An example route that requires a valid access token for authentication, it
+ * will display a boring message to the subject if the middleware successfully
+ * validated the token.
+ */
+app.get('/', authenticationRequired, (req, res) => {
+  res.send('The message of the day is boring: ' + req.jwt.claims.sub)
+});
+
+/**
+ * An example route that requires a valid access token for authentication, it
+ * will echo the contents of the access token if the middleware successfully
+ * validated the token.
+ */
+app.get('/secure', authenticationRequired, (req, res) => {
+  res.json(req.jwt);
+});
+
+/**
+ * Another example route that requires a valid access token for authentication, and
+ * print some messages for the user if they are authenticated
+ */
+app.get('/api/messages', authenticationRequired, (req, res) => {
+  res.json([{
+    message: 'Hello, word!'
+  }]);
+});
+
+app.listen(8080, () => {
+  console.log('Server Ready on port 8080');
+});
