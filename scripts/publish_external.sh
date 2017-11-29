25b1a36c32651c5504c39ad3923378900bb480af
Len Boyette <lboyette@okta.com>
2017-11-28 17:17:10 -0800

    Fix root cause of prepublish failures (#63)

--- scripts/publish_external.sh
+++ scripts/publish_external.sh
@@ -1,2 +1,16 @@
+# undo permissions change on scripts/publish_external.sh
+git checkout -- scripts
+
+# Use newer, faster npm
+npm install -g npm@5.0.3
+
+# Install required dependencies
+npm install -g lerna
+
+if ! lerna bootstrap; then
+  echo "lerna bootstrap failed! Exiting..."
+  exit ${FAILED_SETUP}
+fi
+
 export REGISTRY="https://registry.npmjs.org/"
 node $OKTA_HOME/$REPO/scripts/publish_registry.js
