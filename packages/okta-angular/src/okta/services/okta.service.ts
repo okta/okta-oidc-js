74cf724dd26fa6285b79e01a8c44d977ba188a15
Chris McKnight <cmckni3@gmail.com>
2018-02-21 12:27:23 -0600

    fix(lint): Remove unused import

--- packages/okta-angular/src/okta/services/okta.service.ts
+++ packages/okta-angular/src/okta/services/okta.service.ts
@@ -10,7 +10,7 @@
  * See the License for the specific language governing permissions and limitations under the License.
  */
 
-import { Inject, Injectable, Optional } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { Router, NavigationExtras } from '@angular/router';
 
 import { OKTA_CONFIG, OktaConfig } from '../models/okta.config';
