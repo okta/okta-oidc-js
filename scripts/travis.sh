d14900aca50ce7c8d2c2b50c2d2a16da2be6c47a
Vijet Mahabaleshwar <vijet.mahabaleshwar@okta.com>
2018-03-05 13:34:52 -0800

    Removing lerna test from travis run

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -3,18 +3,9 @@
 npm install
 npm run bootstrap
 
-# npm test
-cd packages/jwt-verifier
-npm test
-
-cd ../oidc-middleware
-npm test
-
-cd ../okta-angular
-npm test
-
-cd ../okta-react
-npm test
-
-cd ../okta-vue
-npm test
+for package in `ls $PWD/packages`;
+do
+    cd $PWD/packages/$package
+    npm test
+    cd ../..
+done
