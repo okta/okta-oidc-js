001852193f3e1c5c0bfb7e75d935b6153bb76f0d
Vijet Mahabaleshwar <vijet.mahabaleshwar@okta.com>
2018-03-09 14:53:22 -0800

    Testing failure with informational message

--- scripts/runTests.sh
+++ scripts/runTests.sh
@@ -7,9 +7,14 @@ do
     cd $PWD/packages/$package
     npm test
     if [ $? -ne 0 ]; then
-       STATUS=-1
+        STATUS=1
+        FAILED_PACKAGE=$package
     fi
     cd ../..
 done
+
+if [ ! -z $FAILED_PACKAGE ]; then
+    echo "------- [ERROR] Test failures in $FAILED_PACKAGE -------"
+fi
 
 exit $STATUS
