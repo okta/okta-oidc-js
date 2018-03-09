45e404c820d6c1df38fd9bcef68448fe45623bc7
Vijet Mahabaleshwar <vijet.mahabaleshwar@okta.com>
2018-03-09 14:24:58 -0800

    Testing new script

--- scripts/runTests.sh
+++ scripts/runTests.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+STATUS=0
+
 for package in `ls $PWD/packages`;
 do
     cd $PWD/packages/$package
     npm test
+    if [ $? -ne 0 ]; then
+       STATUS=-1
+    fi
     cd ../..
 done
+
+exit $STATUS
