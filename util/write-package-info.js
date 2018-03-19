99dc5597f15f0dc511610c9bd028be2fe5d913b2
Robert Damphousse <robert.damphousse.devex@okta.com>
2018-03-19 08:48:53 -0700

    Fix linting issues
    - Add lniter to test in vue
    - Fix issues in new util script

--- util/write-package-info.js
+++ util/write-package-info.js
@@ -3,14 +3,14 @@ const path = require('path');
 
 const workingDirectory = process.argv[2];
 const destinationFile = process.argv[3];
-const packageJsonPath = path.join(process.cwd(), workingDirectory, 'package.json')
+const packageJsonPath = path.join(process.cwd(), workingDirectory, 'package.json');
 const configDest = path.join(process.cwd(), workingDirectory, destinationFile);
 const packageJson = require(packageJsonPath);
 const packageInfo = {
   name: packageJson.name,
   version: packageJson.version
 };
-const output = 'export default ' + JSON.stringify(packageInfo, null, 2) + ';'
+const output = 'export default ' + JSON.stringify(packageInfo, null, 2) + ';';
 
 console.log('Writing config to', configDest);
 
