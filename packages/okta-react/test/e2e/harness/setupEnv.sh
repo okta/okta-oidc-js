0737a31dc7a950fc6202b748eb924bdf44cd6040
Vijet Mahabaleshwar <vijet.mahabaleshwar@okta.com>
2018-04-09 21:13:38 -0700

    Updated react sample to use port 8080

--- packages/okta-react/test/e2e/harness/setupEnv.sh
+++ packages/okta-react/test/e2e/harness/setupEnv.sh
@@ -3,5 +3,5 @@
 if [ $SPA_CLIENT_ID ]
 then
     export CLIENT_ID=$SPA_CLIENT_ID
-    export REDIRECT_URI=http://localhost:3000/implicit/callback
+    export REDIRECT_URI=http://localhost:8080/implicit/callback
 fi
