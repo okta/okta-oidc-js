4e7736221810c78da2b1358fc6851943bcb8d186
Robert Damphousse <robert.damphousse.devex@okta.com>
2018-05-16 16:52:11 -0700

    test [ci-skip]

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -7,7 +7,6 @@ export TEST_SUITE_TYPE="build"
 if [ "$BRANCH" == "master" ];
 then
   echo "attempting npm publish"
-
   export REGISTRY="${ARTIFACTORY_URL}/api/npm/npm-okta"
   node $OKTA_HOME/$REPO/scripts/publish_registry.js
 fi
