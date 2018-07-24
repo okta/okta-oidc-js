1f271991a19b6ddde6443dbc124c52c909228bfe
robertjd <robertjd@users.noreply.github.com>
2018-07-24 11:41:15 -0700

    chore: Run snyk monitor on master commits (#248)
    Run snyk monitor on master commits

---
+++ scripts/snyk.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# When packages change their dependency tree, we want to run snyk monitor. This
+# sends the updated dependency tree to snyk for monitoring.  We want to do this
+# when a commit has been made to master, not on PRs because the dependency tree
+# is likely still in flux while review is happening.
+#
+# For simplicity, this script just runs snyk monitor against all packages on
+# master commits, it doesn't try to figure out which packages were updated.
+
+if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+  npm install -g snyk
+  snyk auth $SNYK_API_TOKEN
+  for package in `ls $PWD/packages`;
+  do
+    echo "snyk monitor --org=$SNYK_ORG_ID"
+  done
+fi
