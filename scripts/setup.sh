6042848a884a04b04fb24dea51f25e06131a2c8a
jmelberg <jordan.melberg@okta.com>
2018-09-26 14:13:09 -0700

    Fix yarn install using internal CI

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -15,7 +15,26 @@ git reset --hard $SHA
 git config --global user.email "oktauploader@okta.com"
 git config --global user.name "oktauploader-okta"
 
-if ! yarn install; then
+#!/bin/bash
+YARN_REGISTRY=https://registry.yarnpkg.com
+OKTA_REGISTRY=${ARTIFACTORY_URL}/api/npm/npm-okta-master
+
+# Yarn does not utilize the npmrc/yarnrc registry configuration
+# if a lockfile is present. This results in `yarn install` problems
+# for private registries. Until yarn@2.0.0 is released, this is our current
+# workaround.
+#
+# Related issues:
+#  - https://github.com/yarnpkg/yarn/issues/5892
+#  - https://github.com/yarnpkg/yarn/issues/3330
+
+# Replace yarn artifactory with Okta's
+sed -i "s#${YARN_REGISTRY}#${OKTA_REGISTRY}#" yarn.lock
+
+if ! yarn install ; then
   echo "yarn install failed! Exiting..."
   exit ${FAILED_SETUP}
 fi
+
+# Revert the origional change
+sed -i "s#${OKTA_REGISTRY}#${YARN_REGISTRY}#" yarn.lock
