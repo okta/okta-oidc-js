0995412a80817f4d69e5a3c46c2ae5ce454571d2
Vijet M <vijet.mahabaleshwar@okta.com>
2018-12-31 15:24:35 -0800

    Updates yarn.lock & adds script to download chromedriver binaries (#360)
    * Updates yarn.lock
    
    * Adds chrome driver binaries
    
    * Downloads chromedriver based on OS
    
    * Adds back integrity values to yarn.lock

--- packages/okta-vue/test/e2e/harness/test/e2e/nightwatch.conf.js
+++ packages/okta-vue/test/e2e/harness/test/e2e/nightwatch.conf.js
@@ -1,6 +1,5 @@
 require('babel-register')
 var config = require('../../config')
-
 // http://nightwatchjs.org/gettingstarted#settings-file
 module.exports = {
   src_folders: ['test/e2e/specs'],
@@ -13,7 +12,7 @@ module.exports = {
     host: '127.0.0.1',
     port: 4444,
     cli_args: {
-      'webdriver.chrome.driver': require('chromedriver').path
+      'webdriver.chrome.driver': './test/e2e/lib/chromedriver'
     }
   },
 
