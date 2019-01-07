378b48595a7e545bed11ae0346b3e84bceb9e9cf
Vijet Mahabaleshwar <vijet.mahabaleshwar@okta.com>
2019-01-07 13:45:57 -0800

    Adds junit reports to all packages

--- packages/okta-vue/test/e2e/harness/test/e2e/nightwatch.conf.js
+++ packages/okta-vue/test/e2e/harness/test/e2e/nightwatch.conf.js
@@ -3,7 +3,7 @@ var config = require('../../config')
 // http://nightwatchjs.org/gettingstarted#settings-file
 module.exports = {
   src_folders: ['test/e2e/specs'],
-  output_folder: 'test/e2e/reports',
+  output_folder: '../../../reports/junit',
   custom_assertions_path: ['test/e2e/custom-assertions'],
 
   selenium: {
