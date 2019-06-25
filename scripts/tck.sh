d518530035b78649a32442f5d91904be3461778d
Vijet Mahabaleshwar <vijet.mahabaleshwar@okta.com>
2019-06-25 12:47:36 -0700

    Updates TCK version to 0.5.4 to get tests passing!

--- scripts/tck.sh
+++ scripts/tck.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TCK_VERSION=0.4.0-SNAPSHOT
+TCK_VERSION=0.5.4-SNAPSHOT
 TCK_JAR_URL="https://oss.sonatype.org/service/local/artifact/maven/redirect?r=public&g=com.okta.oidc.tck&a=okta-oidc-tck&v=${TCK_VERSION}&e=jar&c=shaded"
 TCK_FILE="./okta-oidc-tck-${TCK_VERSION}-shaded.jar"
 TCK_PEM="./tck-keystore.pem"
@@ -9,6 +9,8 @@ function runTest() {
     test_runner_yml=$1; #"./test/integration-test/resources/testRunner.yml"
     test_output_dir=$2; #"target/cli-test-output"
     testng_xml=$3; #"./test/integration-test/resources/testng.xml"
+
+    echo "test_output_dir: " $2
 
     echo "TCK_FILE:"
 
@@ -19,7 +21,7 @@ function runTest() {
 
     mkdir -p target
     #JAVA_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=8000"
-    java ${JAVA_OPTS} -Dconfig="${test_runner_yml}" -jar "${TCK_FILE}" -d "${test_output_dir}" "${testng_xml}"
+    java ${JAVA_OPTS} -Dokta.testing.disableHttpsCheck=true -Dconfig="${test_runner_yml}" -jar "${TCK_FILE}" -d "${test_output_dir}" "${testng_xml}"
 
     return_status=$?
 
