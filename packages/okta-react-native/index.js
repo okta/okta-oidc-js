e96c4f7820ae877309f890dc8f8887db7b7724a4
Kayci Wang <kitty7756@hotmail.com>
2019-06-26 16:23:34 -0400

    Fixes linting of JS file

--- packages/okta-react-native/index.js
+++ packages/okta-react-native/index.js
@@ -1,94 +1,94 @@
 import { NativeModules, Platform } from 'react-native';
 import {
-    assertIssuer,
-    assertClientId,
-    assertRedirectUri
-  } from '@okta/configuration-validation';
+	assertIssuer,
+  assertClientId,
+	assertRedirectUri
+} from '@okta/configuration-validation';
 import jwt from 'jwt-lite';
 
 export const createConfig = async (
-    clientId,
-    redirectUri, 
-    endSessionRedirectUri, 
-    discoveryUri,
-    scopes) => {
+  clientId,
+  redirectUri, 
+  endSessionRedirectUri, 
+  discoveryUri,
+  scopes) => {
 
-    assertIssuer(discoveryUri);
-    assertClientId(clientId);
-    assertRedirectUri(redirectUri);
-    assertRedirectUri(endSessionRedirectUri);
+  assertIssuer(discoveryUri);
+  assertClientId(clientId);
+  assertRedirectUri(redirectUri);
+  assertRedirectUri(endSessionRedirectUri);
 
-    if (Platform.OS === 'ios') {
-        scopes = scopes.join();
-        scopes = scopes.replace(/,/g, ' ');
-    }
+  if (Platform.OS === 'ios') {
+		scopes = scopes.join();
+		scopes = scopes.replace(/,/g, ' ');
+  }
     
-    return NativeModules.OktaSdkBridge.createConfig(
-        clientId,
-        redirectUri,
-        endSessionRedirectUri,
-        discoveryUri,
-        scopes
-        );
+  return NativeModules.OktaSdkBridge.createConfig(
+    clientId,
+    redirectUri,
+    endSessionRedirectUri,
+    discoveryUri,
+    scopes
+  );
 } 
 
 export const signIn = async () => {
-    return NativeModules.OktaSdkBridge.signIn();
+  return NativeModules.OktaSdkBridge.signIn();
 }
 
 export const signOut = async () => {
-    return NativeModules.OktaSdkBridge.signOut();
+  return NativeModules.OktaSdkBridge.signOut();
 }
 
 export const getAccessToken = async () => {
-    return NativeModules.OktaSdkBridge.getAccessToken();
+  return NativeModules.OktaSdkBridge.getAccessToken();
 }
 
 export const getIdToken = async () => {
-    return NativeModules.OktaSdkBridge.getIdToken();
+  return NativeModules.OktaSdkBridge.getIdToken();
 }
 
 export const getUser = async () => {
-    return NativeModules.OktaSdkBridge.getUser();
+  return NativeModules.OktaSdkBridge.getUser();
 }
 
 export const getUserFromIdToken = async() => {
-    try {
-        let idTokenResponse = await getIdToken();
-        return jwt.decode(idTokenResponse.id_token).claimsSet;
-    } catch(error) {
-        throw error;
-    } 
+  try {
+    let idTokenResponse = await getIdToken();
+    return jwt.decode(idTokenResponse.id_token).claimsSet;
+  } catch(error) {
+    throw error;
+  } 
 }
 
 export const isAuthenticated = async () => {
-    return NativeModules.OktaSdkBridge.isAuthenticated();
+  return NativeModules.OktaSdkBridge.isAuthenticated();
 }
 
 export const revokeAccessToken = async() => {
-    return NativeModules.OktaSdkBridge.revokeAccessToken();
+  return NativeModules.OktaSdkBridge.revokeAccessToken();
 }
 
 export const revokeIdToken = async() => {
-    return NativeModules.OktaSdkBridge.revokeIdToken();
+  return NativeModules.OktaSdkBridge.revokeIdToken();
 }
 
 export const revokeRefreshToken = async() => {
-    return NativeModules.OktaSdkBridge.revokeRefreshToken();
+  return NativeModules.OktaSdkBridge.revokeRefreshToken();
 }
 
 export const introspectAccessToken = async() => {
-    return NativeModules.OktaSdkBridge.introspectAccessToken(); 
+  return NativeModules.OktaSdkBridge.introspectAccessToken(); 
 }
 
 export const introspectIdToken = async() => {
-    return NativeModules.OktaSdkBridge.introspectIdToken(); 
+  return NativeModules.OktaSdkBridge.introspectIdToken(); 
 }
 
 export const introspectRefreshToken = async() => {
-    return NativeModules.OktaSdkBridge.introspectRefreshToken(); 
+  return NativeModules.OktaSdkBridge.introspectRefreshToken(); 
 }
 
 export const refreshTokens = async() => {
-    return NativeModules.OktaSdkBridge.refreshTokens(); 
+  return NativeModules.OktaSdkBridge.refreshTokens(); 
 }
