f43160d2332cfe3a34327ff343e35043e980bff9
Vijet Mahabaleshwar <vijet.mahabaleshwar@okta.com>
2019-08-08 11:04:16 -0700

    Enabling all tests

--- scripts/runTests.sh
+++ scripts/runTests.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
-# for package in `ls $PWD/packages`;
-# do
-#     cd $PWD/packages/$package
-#     yarn test
-#     if [ $? -ne 0 ]; then
-#         echo "------- [ERROR] Test failures in $package -------"
-#         exit 1
-#     fi
-#     cd ../..
-# done
-
-cd $PWD/packages/okta-react
-yarn test
-if [ $? -ne 0 ]; then
-    echo "------- [ERROR] Test failures in okta-rect -------"
-    exit 1
-fi
+for package in `ls $PWD/packages`;
+do
+    cd $PWD/packages/$package
+    yarn test
+    if [ $? -ne 0 ]; then
+        echo "------- [ERROR] Test failures in $package -------"
+        exit 1
+    fi
+    cd ../..
+done
