1c39da108b23b897698a39f0892cc57342ba6bc5
Fei Chen <fei.chen@okta.com>
2019-08-21 17:29:34 -0700

    Update to androidx to be compatible with 0.60 (#533)

--- packages/okta-react-native/android/src/main/java/com/oktareactnative/OktaSdkBridgeModule.java
+++ packages/okta-react-native/android/src/main/java/com/oktareactnative/OktaSdkBridgeModule.java
@@ -15,8 +15,8 @@ package com.oktareactnative;
 import android.app.Activity;
 import android.content.Intent;
 
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.facebook.react.bridge.ActivityEventListener;
 import com.facebook.react.bridge.Arguments;
