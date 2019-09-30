9111ea1f64cdccfcfc362a4da1efc2a0f8767d01
Aaron Granick <aaron.granick@okta.com>
2019-09-30 09:54:34 -0700

    ng lint fix

--- scripts/installPackages.sh
+++ scripts/installPackages.sh
@@ -13,9 +13,10 @@ fi
 # Install dependencies and run the 'prepare' script
 # Running yarn in each package directory to create a unique yarn.lock file
 npx lerna exec --concurrency=1 -- yarn install $YARN_INSTALL_ARGS
-npx lerna run prepare
 
 # Calling 'yarn install' within a package directory may install a hard copy from npm repository
 # Overwrite a hard copy with a symlink. This ensures we are always running and testing against the local version
 npx lerna link --force-local
 
+# Run build scripts
+npx lerna run prepare
