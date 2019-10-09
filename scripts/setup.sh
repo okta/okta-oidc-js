0cc6469e0df9f78cd9d402f8e6fd592a3baa85b4
Aaron Granick <aaron.granick@okta.com>
2019-10-09 11:32:57 -0700

    Provide cafile (from mozilla)

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -16,8 +16,8 @@ git config --global user.email "oktauploader@okta.com"
 git config --global user.name "oktauploader-okta"
 
 #!/bin/bash
-YARN_REGISTRY=https://registry.yarnpkg.com
-OKTA_REGISTRY=${ARTIFACTORY_URL}/api/npm/npm-okta-master
+# YARN_REGISTRY=https://registry.yarnpkg.com
+# OKTA_REGISTRY=${ARTIFACTORY_URL}/api/npm/npm-okta-master
 
 # Yarn does not utilize the npmrc/yarnrc registry configuration
 # if a lockfile is present. This results in `yarn install` problems
@@ -29,18 +29,18 @@ OKTA_REGISTRY=${ARTIFACTORY_URL}/api/npm/npm-okta-master
 #  - https://github.com/yarnpkg/yarn/issues/3330
 
 # Replace yarn registry with Okta's
-echo "Replacing $YARN_REGISTRY with $OKTA_REGISTRY within yarn.lock files..."
-sed -i "s#${YARN_REGISTRY}#${OKTA_REGISTRY}#" yarn.lock
-sed -i "s#${YARN_REGISTRY}#${OKTA_REGISTRY}#" packages/okta-angular/test/e2e/harness/yarn.lock
-sed -i "s#${YARN_REGISTRY}#${OKTA_REGISTRY}#" packages/okta-react/test/e2e/harness/yarn.lock
-sed -i "s#${YARN_REGISTRY}#${OKTA_REGISTRY}#" packages/okta-vue/test/e2e/harness/yarn.lock
-sed -i "s#${YARN_REGISTRY}#${OKTA_REGISTRY}#" packages/configuration-validation/yarn.lock
-sed -i "s#${YARN_REGISTRY}#${OKTA_REGISTRY}#" packages/jwt-verifier/yarn.lock
-sed -i "s#${YARN_REGISTRY}#${OKTA_REGISTRY}#" packages/oidc-middleware/yarn.lock
-sed -i "s#${YARN_REGISTRY}#${OKTA_REGISTRY}#" packages/okta-angular/yarn.lock
-sed -i "s#${YARN_REGISTRY}#${OKTA_REGISTRY}#" packages/okta-react/yarn.lock
-sed -i "s#${YARN_REGISTRY}#${OKTA_REGISTRY}#" packages/okta-vue/yarn.lock
-sed -i "s#${YARN_REGISTRY}#${OKTA_REGISTRY}#" packages/okta-react-native/yarn.lock
+# echo "Replacing $YARN_REGISTRY with $OKTA_REGISTRY within yarn.lock files..."
+# sed -i "s#${YARN_REGISTRY}#${OKTA_REGISTRY}#" yarn.lock
+# sed -i "s#${YARN_REGISTRY}#${OKTA_REGISTRY}#" packages/okta-angular/test/e2e/harness/yarn.lock
+# sed -i "s#${YARN_REGISTRY}#${OKTA_REGISTRY}#" packages/okta-react/test/e2e/harness/yarn.lock
+# sed -i "s#${YARN_REGISTRY}#${OKTA_REGISTRY}#" packages/okta-vue/test/e2e/harness/yarn.lock
+# sed -i "s#${YARN_REGISTRY}#${OKTA_REGISTRY}#" packages/configuration-validation/yarn.lock
+# sed -i "s#${YARN_REGISTRY}#${OKTA_REGISTRY}#" packages/jwt-verifier/yarn.lock
+# sed -i "s#${YARN_REGISTRY}#${OKTA_REGISTRY}#" packages/oidc-middleware/yarn.lock
+# sed -i "s#${YARN_REGISTRY}#${OKTA_REGISTRY}#" packages/okta-angular/yarn.lock
+# sed -i "s#${YARN_REGISTRY}#${OKTA_REGISTRY}#" packages/okta-react/yarn.lock
+# sed -i "s#${YARN_REGISTRY}#${OKTA_REGISTRY}#" packages/okta-vue/yarn.lock
+# sed -i "s#${YARN_REGISTRY}#${OKTA_REGISTRY}#" packages/okta-react-native/yarn.lock
 
 if ! yarn install ; then
   echo "yarn install failed! Exiting..."
@@ -48,15 +48,15 @@ if ! yarn install ; then
 fi
 
 # Revert the original change(s)
-echo "Replacing $OKTA_REGISTRY with $YARN_REGISTRY within yarn.lock files..."
-sed -i "s#${OKTA_REGISTRY}#${YARN_REGISTRY}#" yarn.lock
-sed -i "s#${OKTA_REGISTRY}#${YARN_REGISTRY}#" packages/okta-angular/test/e2e/harness/yarn.lock
-sed -i "s#${YARN_REGISTRY}#${OKTA_REGISTRY}#" packages/okta-react/test/e2e/harness/yarn.lock
-sed -i "s#${OKTA_REGISTRY}#${YARN_REGISTRY}#" packages/okta-vue/test/e2e/harness/yarn.lock
-sed -i "s#${OKTA_REGISTRY}#${YARN_REGISTRY}#" packages/configuration-validation/yarn.lock
-sed -i "s#${OKTA_REGISTRY}#${YARN_REGISTRY}#" packages/jwt-verifier/yarn.lock
-sed -i "s#${OKTA_REGISTRY}#${YARN_REGISTRY}#" packages/oidc-middleware/yarn.lock
-sed -i "s#${OKTA_REGISTRY}#${YARN_REGISTRY}#" packages/okta-angular/yarn.lock
-sed -i "s#${OKTA_REGISTRY}#${YARN_REGISTRY}#" packages/okta-react/yarn.lock
-sed -i "s#${OKTA_REGISTRY}#${YARN_REGISTRY}#" packages/okta-vue/yarn.lock
-sed -i "s#${OKTA_REGISTRY}#${YARN_REGISTRY}#" packages/okta-react-native/yarn.lock
+# echo "Replacing $OKTA_REGISTRY with $YARN_REGISTRY within yarn.lock files..."
+# sed -i "s#${OKTA_REGISTRY}#${YARN_REGISTRY}#" yarn.lock
+# sed -i "s#${OKTA_REGISTRY}#${YARN_REGISTRY}#" packages/okta-angular/test/e2e/harness/yarn.lock
+# sed -i "s#${YARN_REGISTRY}#${OKTA_REGISTRY}#" packages/okta-react/test/e2e/harness/yarn.lock
+# sed -i "s#${OKTA_REGISTRY}#${YARN_REGISTRY}#" packages/okta-vue/test/e2e/harness/yarn.lock
+# sed -i "s#${OKTA_REGISTRY}#${YARN_REGISTRY}#" packages/configuration-validation/yarn.lock
+# sed -i "s#${OKTA_REGISTRY}#${YARN_REGISTRY}#" packages/jwt-verifier/yarn.lock
+# sed -i "s#${OKTA_REGISTRY}#${YARN_REGISTRY}#" packages/oidc-middleware/yarn.lock
+# sed -i "s#${OKTA_REGISTRY}#${YARN_REGISTRY}#" packages/okta-angular/yarn.lock
+# sed -i "s#${OKTA_REGISTRY}#${YARN_REGISTRY}#" packages/okta-react/yarn.lock
+# sed -i "s#${OKTA_REGISTRY}#${YARN_REGISTRY}#" packages/okta-vue/yarn.lock
+# sed -i "s#${OKTA_REGISTRY}#${YARN_REGISTRY}#" packages/okta-react-native/yarn.lock
