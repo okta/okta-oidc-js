aa3b1540a840cac7526316a2452b8bd8c689620b
Aaron Granick <aaron.granick@okta.com>
2020-03-06 15:25:08 -0800

    chore: use ci-update-package for npm publish (#687)

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -13,13 +13,58 @@ ln -sf ~/.yarn/bin/yarn /usr/bin/yarn
 source $OKTA_HOME/$REPO/scripts/setup.sh
 
 export TEST_SUITE_TYPE="build"
+export REGISTRY="${ARTIFACTORY_URL}/api/npm/npm-okta"
 
-if [ "$BRANCH" == "master" ];
+# Install required dependencies
+export PATH="${PATH}:$(yarn global bin)"
+yarn global add @okta/ci-update-package
+yarn global add @okta/ci-pkginfo
+
+if [ -n "${action_branch}" ];
 then
-  echo "attempting npm publish"
-
-  export REGISTRY="${ARTIFACTORY_URL}/api/npm/npm-okta"
-  node $OKTA_HOME/$REPO/scripts/publish_registry.js
+  echo "Publishing from bacon task using branch ${action_branch}"
+  TARGET_BRANCH=${action_branch}
+else
+  echo "Publishing from bacon testSuite using branch ${BRANCH}"
+  TARGET_BRANCH=${BRANCH}
 fi
+
+PACKAGES=(
+  "./packages/configuration-validation"
+  "./packages/jwt-verifier"
+  "./packages/oidc-middleware"
+  "./packages/okta-angular/dist"
+  "./packages/okta-react"
+  "./packages/okta-react-native"
+  "./packages/okta-vue"
+)
+
+for PACKAGE in "${PACKAGES[@]}"
+do
+  pushd $PACKAGE
+
+  if ! ci-update-package --branch ${TARGET_BRANCH}; then
+    echo "ci-update-package failed for $PACKAGE! Exiting..."
+    exit ${FAILED_SETUP}
+  fi
+
+  ### looks like ci-update-package is not compatible with `yarn publish`
+  ### which expects new-version is passed via command line parameter.
+  ### keep using npm for now
+  if ! npm publish --registry ${REGISTRY}; then
+    echo "npm publish failed for $PACKAGE! Exiting..."
+    exit ${PUBLISH_ARTIFACTORY_FAILURE}
+  fi
+
+
+  DATALOAD=$(ci-pkginfo -t dataload)
+  if ! artifactory_curl -X PUT -u ${ARTIFACTORY_CREDS} ${DATALOAD} -v -f; then
+    echo "artifactory_curl failed for $PACKAGE! Exiting..."
+    exit ${PUBLISH_ARTIFACTORY_FAILURE}
+  fi
+
+  popd
+
+done
 
 exit $SUCCESS
