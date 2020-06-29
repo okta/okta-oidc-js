163805c610eaefbb60048d2b7cb72878650edcfa
Shuo Wu <wushuo2010@gmail.com>
2020-06-29 15:12:21 -0400

    fix[configuration-validation]: move lodash to dependencies - OKTA-300101

--- packages/configuration-validation/src/lib.js
+++ packages/configuration-validation/src/lib.js
@@ -10,7 +10,7 @@
  * See the License for the specific language governing permissions and limitations under the License.
  */
 
-const merge = require('lodash/merge');
+const merge = require('lodash.merge');
 class ConfigurationValidationError extends Error {}
 
 const configUtil = module.exports;
