0d1ef1499e3dc1dfc4c66f3b9a53c08bee09a182
Shuo Wu <wushuo2010@gmail.com>
2021-06-15 13:52:01 -0400

    chore: split unit test as separate script

---
+++ scripts/unit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -xe
+
+source ${OKTA_HOME}/${REPO}/scripts/setup.sh
+
+PACKAGES=(
+  "./packages/configuration-validation"
+  "./packages/jwt-verifier"
+  "./packages/oidc-middleware"
+)
+
+for PACKAGE in "${PACKAGES[@]}"
+do
+    pushd $PACKAGE
+    yarn test:unit
+    if [ $? -ne 0 ]; then
+        echo "------- [ERROR] Unit Test failures in $PACKAGE -------"
+        exit 1
+    fi
+    popd
+done
